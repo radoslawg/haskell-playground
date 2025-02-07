db30bbc39fba55f5e49b40993c6130839605a660
Radosław Grzanka <radoslawg@gmail.com>
2025-02-07 20:53:40 +0000

    Folding exercises

--- app/List.hs
+++ app/List.hs
@@ -39,6 +39,18 @@ hasPath' xs x y
 sum' :: [Int] -> Int
 sum' = foldr (+) 0
 
+rev' :: [a] -> [a]
+rev' = foldl (\x z -> z:x) []
+
+rev'' :: [a] -> [a]
+rev'' = foldl (flip (:)) []
+
+-- Create a function that will return prefixes
+-- i.e prefixes [1, 2, 3] => [[1], [1, 2], [1, 2, 3]]
+prefixes :: [a] -> [[a]]
+prefixes y = rev'' $ foldl (\x z -> (take (length x) y++[z]) :x) [] y
+prefixes' = foldr(\x acc -> [x] : (map ((:) x) acc)) []
+
 main :: IO ()
 main = do
     print (elem' 3 [1,2,3])
@@ -47,3 +59,7 @@ main = do
     print (hasPath [(1, 2), (2, 3), (3, 2), (4, 3), (4, 5)] 5 1)
     print (hasPath' [(1, 2), (2, 3), (3, 2), (4, 3), (4, 5)] 5 1)
     print (sum' [1, 2, 3, 4])
+    print $ rev' [1,2,3]
+    print $ rev'' [1,2,3]
+    print (prefixes [1, 2, 3])
+    print (prefixes' [1, 2, 3])
